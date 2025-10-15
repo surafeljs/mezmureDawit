748d804cbe77c440e5d5c376ba647b9740c2c8fd
surafeljs <surafel.node@gmail.com>
2025-10-15 02:49:52 -0700

    modified

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -13,11 +13,27 @@ class _DrawersState extends State<Drawers> {
     return Drawer(
       child: ListView(
         children: [
-          DrawerHeader(child: Icon(Icons.ac_unit)),
+          DrawerHeader(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/134009274249421277.jpg'),
 
+                fit: BoxFit.cover,
+                // Makes the image fill the header
+              ),
+            ),
+            child: Container(), // You can add overlay widgets here if needed
+          ),
+          SizedBox(height: 20.0),
           ListTile(leading: Icon(Icons.home), title: Text('Home')),
+          SizedBox(height: 20.0),
+
           ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+          SizedBox(height: 20.0),
+
           ListTile(leading: Icon(Icons.info), title: Text('About')),
+          SizedBox(height: 20.0),
+
           ListTile(leading: Icon(Icons.logout), title: Text('Logout')),
         ],
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:orthodox/drawer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(Orthodox());
@@ -13,13 +14,113 @@ class Orthodox extends StatefulWidget {
 }
 
 class _OrthodoxState extends State<Orthodox> {
+  String text = '';
+  final _controller = TextEditingController();
+  bool isThems = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getDatas();
+  }
+
+  Future<void> setDatas() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    SharedPreferences prefsText = await SharedPreferences.getInstance();
+
+    prefs.setBool('isDartMode', isThems);
+    prefsText.setString('str', text);
+  }
+
+  Future<void> getDatas() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    SharedPreferences prefsText = await SharedPreferences.getInstance();
+
+    bool? them = prefs.getBool('isDartMode') ?? false;
+    String? getText = prefsText.getString('str') ?? ' ';
+
+    print(them);
+    print(getText);
+
+    setState(() {
+      isThems = them;
+      text = getText;
+    });
+  }
+
+  var _currentIndex = 1;
+  void activity(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> lists = [
+      Container(child: Text(text.isEmpty ? 'No text entered' : text)),
+      Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 50.0, right: 50.0),
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  labelText: 'User name',
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _controller.clear(); // Clear text field
+                      });
+                    },
+                    icon: Icon(Icons.clear),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              child: Text('Btn'),
+              onPressed: () {
+                setState(() {
+                  text = _controller.text;
+                });
+                setDatas();
+              },
+            ),
+          ],
+        ),
+      ),
+      Container(child: Text('dfgata')),
+    ];
     return MaterialApp(
+      theme: isThems ? ThemeData.dark() : ThemeData.light(),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.deepOrange),
+        appBar: AppBar(
+          actions: [
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Switch(
+                  value: isThems,
+                  onChanged: (value) {
+                    setState(() {
+                      isThems = value;
+                    });
+                    setDatas();
+                  },
+                ),
+              ),
+            ),
+          ],
+
+          backgroundColor: Colors.deepOrange,
+        ),
         drawer: Drawers(),
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: activity,
           type: BottomNavigationBarType.fixed,
           items: [
             BottomNavigationBarItem(
@@ -36,14 +137,7 @@ class _OrthodoxState extends State<Orthodox> {
             ),
           ],
         ),
-        body: SafeArea(
-          child: TextField(
-            onChanged: (value) {
-              setState(() {});
-            },
-            decoration: InputDecoration(),
-          ),
-        ),
+        body: lists[_currentIndex],
       ),
     );
   }
