284ed6438b69259463d13535cbe369f7745eb3e7
surafeljs <surafel.node@gmail.com>
2026-01-21 05:06:01 -0800

    audio_part

--- lib/audio_playlist.dart
+++ lib/audio_playlist.dart
@@ -20,11 +20,53 @@ class _PlaylistState extends State<Playlist> {
         backgroundColor: Colors.orange[400],
       ),
       body: SafeArea(
-        child: Center(
-          child: Hero(
-            tag: 'audio_playlist',
-            child: Container(width: 200.0, height: 200.0, color: Colors.red),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20.0),
+              child: Hero(
+                tag: 'audio_playlist',
+                child: Container(
+                  width: 200.0,
+                  height: 200.0,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: const Color.fromARGB(107, 218, 212, 193),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 40.0,
+                  children: [
+                    IconButton.outlined(
+                      onPressed: () {},
+                      icon: Icon(Icons.skip_previous, color: Colors.deepOrange),
+                    ),
+                    IconButton.outlined(
+                      onPressed: () {},
+                      icon: Icon(
+                        size: 40.0,
+                        Icons.play_arrow, //Icons.pause
+                        color: Colors.deepOrange,
+                      ),
+                    ),
+                    IconButton.outlined(
+                      onPressed: () {},
+                      icon: Icon(Icons.skip_next, color: Colors.deepOrange),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
